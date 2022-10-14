754da1997a2a3bd4fc9e25f971873a3404c98aea
bnomartins23 <31700287+bnomartins23@users.noreply.github.com>
2022-10-14 07:55:20 -0300

    Atualização do frontend

--- dao/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hostname = "localhost";
-$bancodedados = "mural";
-$usuario = "root";
-$senha = "";
-
-$query = new mysqli($hostname, $usuario, $senha, $bancodedados) or print(mysqli_error());
-?>
